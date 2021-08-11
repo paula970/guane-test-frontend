f325f8f0cb8ec533f7e8d74ca8294d110623ab85
paula970 <paulamp970@gmail.com>
2021-08-11 16:28:40 -0500

    improvement: control the number of pages in the component

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="pagination">
-    <button v-on:click="changePage(-10)">Previus</button>
-    <button v-on:click="changePage(+10)">Next</button>
+    <button v-bind:disabled="page == 1" v-on:click="changePage(-10)">Previus</button>
+    <button v-bind:disabled="page == 5" v-on:click="changePage(+10)">Next</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Pagination",
+  computed: {
+    page() {
+      return this.$store.getters.page;
+    },
+  },
   methods: {
     changePage(param) {
+      let actualPage= this.$store.getters.page
+      let nextPage = param == 10 ? ++actualPage : --actualPage  
       let arrayids = this.$store.getters.ids.map((ids) => {
         return ids + param;
       });
+      this.$store.dispatch("changePageAction", nextPage);
       this.$store.dispatch("changeIdsAction", arrayids);
       this.$store.dispatch("getCharactersAction");
     },
