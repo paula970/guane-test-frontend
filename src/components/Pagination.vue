de8da5eb5d9d0ee0107398619f0bae6dd3cfb773
paula970 <paulamp970@gmail.com>
2021-08-11 03:39:57 -0500

    feature: create pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pagination">
+    <button v-on:click="changePage(-10)">Previus</button>
+    <button v-on:click="changePage(+10)">Next</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pagination",
+  methods: {
+    changePage(param) {
+      let arrayids = this.$store.getters.ids.map((ids) => {
+        return ids + param;
+      });
+      this.$store.dispatch("changeIdsAction", arrayids);
+      this.$store.dispatch("getCharactersAction");
+    },
+  },
+};
+</script>
