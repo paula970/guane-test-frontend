189beaeec5aafe5b8bb48c651e509e2d335b905b
paula970 <paulamp970@gmail.com>
2021-08-11 03:34:01 -0500

    feature: create DetailsCharacter component

---
+++ src/components/DetailsCharacter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-show="character" class="details">
+    <h3>Name</h3>
+    {{ character.name }}
+    <h3>Gender</h3>
+    {{ character.gender }}
+    <button v-on:click="goBack()">Return</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailsCharacter",
+  methods: {
+    goBack() {
+      this.$router.push("/");
+    },
+  },
+  computed: {
+    character() {
+      return this.$store.getters.infoCharacter;
+    },
+  },
+};
+</script>
