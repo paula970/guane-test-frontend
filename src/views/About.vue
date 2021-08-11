307991f9acac70c30b06bf7e47f01930194b7bb3
paula970 <paulamp970@gmail.com>
2021-08-11 03:36:10 -0500

    hotfix: rename the About folder and implement the DetailsCharacter component

--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about pagesss</h1>
-  </div>
-</template>
---
+++ src/views/AboutCharacter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <DetailsCharacter />
+  </div>
+</template>
+
+<script>
+import DetailsCharacter from "@/components/DetailsCharacter.vue";
+
+export default {
+  name: "AboutCharacter",
+  components: {
+    DetailsCharacter,
+  },
+};
+</script>
