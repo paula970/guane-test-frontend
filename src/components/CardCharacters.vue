21126a000c9c876c45337ea82b8e2d20cc3d526c
paula970 <paulamp970@gmail.com>
2021-08-12 23:07:27 -0500

    feature: add styles and functionalities to the CardCharacters component

--- src/components/CardCharacters.vue
+++ src/components/CardCharacters.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="card">
     <div v-for="character in characters" :key="character.id">
-      <h3>Name</h3>
-      {{ character.name }}
-      <h3>Gender</h3>
-      {{ character.gender }}
-      <button v-on:click="moreInfo(character.id)">more information</button>
+      <router-link
+        tag="div"
+        class="card-character"
+        :to="{ name: 'about-the-character', params: { id: character.id } }"
+      >
+        <img
+          class="card-character__image"
+          v-bind:src="character.image"
+          v-bind:alt="image"
+        />
+        <div class="card-character__info">
+          <h4>Name</h4>
+          <h5>{{ character.name }}</h5>
+          <h4>Species</h4>
+          <h5>{{ character.species }}</h5>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -13,12 +25,6 @@
 <script>
 export default {
   name: "CardCharacters",
-  methods: {
-    moreInfo(id) {
-      this.$store.dispatch("getMoreInfoAction", id);
-      this.$router.push("about-the-character");
-    },
-  },
   computed: {
     characters() {
       return this.$store.getters.characters;
@@ -29,3 +35,37 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.card {
+  flex-direction: unset !important;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  margin: 2rem;
+  background-color: unset !important;
+  border: unset !important;
+}
+.card-character {
+  width: 100%;
+  max-width: 80%;
+  border-radius: 1rem;
+  background: #35c9dd;
+  box-shadow: 0 7px 15px -5px rgba(0, 151, 206, 76);
+  border: unset !important;
+  margin: 1em auto;
+
+  &:hover {
+    box-shadow: 0 15px 25px -5px rgba(0, 151, 206, 76);
+    cursor: pointer;
+  }
+
+  &__image {
+    width: 100%;
+    border-top-left-radius: 1em;
+    border-top-right-radius: 1rem;
+  }
+  h4 {
+    font-weight: bold;
+  }
+}
+</style>
